51e5a6820f48fdb765e611238e8165dae423db76
yonatan.cohavi <yonatan.cohavi@gmail.com>
2021-03-13 19:14:13 +0200

    added unitTests

---
+++ Tests/Models/ContactModel.cs
@@ -0,0 +1,35 @@
+﻿using EntityMapper;
+using EntityMapper.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Models
+{
+    public enum ContactMood
+    {
+        Sad = 0,
+        Tired = 1,
+        Happy = 2,
+        Coding = 14,
+    }
+    public class ContactModel : CrmEntityBase
+    {
+       
+        public override string LogicalName => "contact";
+
+        [CRMField("fullname")]
+        public string FullName { get; set; }
+
+        [CRMLookup("parentaccountid", "account")]
+        public Guid? ParentAccountId { get; set; }
+        [CRMField("parentaccountid", CRMFieldType.FormattedValue)]
+        public string ParentAccountIdName { get; set; }
+        [CRMField("parentaccountid", CRMFieldType.LookupName)]
+        public string ParentAccountIdLookupName { get; set; }
+        [CRMEnum("new_mood", typeof(ContactMood?))]
+        public ContactMood? Mood { get; set; }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using EntityMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Linq;
+using Tests.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class GeneralTests
+    {
+        [TestMethod]
+        public void MapFromEntity()
+        {
+            var guid = Guid.NewGuid();
+            var accountRef = new EntityReference("account", guid)
+            {
+                Name = "ParentAccountIdLookupName"
+            };
+            var mood = ContactMood.Coding;
+            var contactEntity = new Entity("contact")
+            {
+                ["fullname"] = "John Doe",
+                ["parentaccountid"] = accountRef,
+                ["new_mood"] = new OptionSetValue((int)mood),
+            };
+
+            // faking formatted value
+            contactEntity.FormattedValues["parentaccountid"] = "Mama Theresa";
+
+            var contactModel = Mapper.ToModel<ContactModel>(contactEntity);
+            Assert.AreEqual("John Doe", contactModel.FullName);
+            Assert.AreEqual(guid, contactModel.ParentAccountId);
+            Assert.AreEqual("Mama Theresa", contactModel.ParentAccountIdName);
+            Assert.AreEqual("ParentAccountIdLookupName", contactModel.ParentAccountIdLookupName);
+            Assert.AreEqual(contactModel.Mood, mood);
+            Assert.AreEqual(contactModel.Mood.GetType().FullName, typeof(ContactMood).FullName);
+        }
+
+        [TestMethod]
+        public void MapToEntity()
+        {
+            var mood = ContactMood.Coding;
+            var contactModel = new ContactModel
+            {
+                FullName = "Bar Refaeli",
+                ParentAccountId = Guid.Empty,
+                Mood = mood
+            };
+
+            var contactEntity = Mapper.ToEntity(contactModel);
+
+            var fullname = contactEntity.GetAttributeValue<string>("fullname");
+            var parentAccountRef = contactEntity.GetAttributeValue<EntityReference>("parentaccountid");
+            var moodValue = contactEntity.GetAttributeValue<OptionSetValue>("new_mood").Value;
+
+            Assert.AreEqual(contactModel.FullName, fullname);
+            Assert.AreEqual("account", parentAccountRef.LogicalName);
+            Assert.AreEqual(contactModel.ParentAccountId, parentAccountRef.Id);
+            Assert.AreEqual(contactModel.Mood, (ContactMood)moodValue);
+            Assert.AreEqual(contactModel.Mood.GetType().FullName, typeof(ContactMood).FullName);
+        }
+
+        [TestMethod]
+        public void GetColumnSet()
+        {
+            var contactColumnSet = Mapper.GetColumnSet<ContactModel>();
+            var expectedColumns = new[] { "fullname", "parentaccountid", "new_mood" };
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedColumns, contactColumnSet.Columns));
+        }
+    }
+}
